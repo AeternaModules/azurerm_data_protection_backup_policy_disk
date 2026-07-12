e93e2cf533a55e70b4b1fb6aa1c94c1dead38a92
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "data_protection_backup_policy_disks" {
-  description = "All data_protection_backup_policy_disk resources"
-  value       = azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks
-}
 output "data_protection_backup_policy_disks_backup_repeating_time_intervals" {
-  description = "List of backup_repeating_time_intervals values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.backup_repeating_time_intervals]
+  description = "Map of backup_repeating_time_intervals values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.backup_repeating_time_intervals }
 }
 output "data_protection_backup_policy_disks_default_retention_duration" {
-  description = "List of default_retention_duration values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.default_retention_duration]
+  description = "Map of default_retention_duration values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.default_retention_duration }
 }
 output "data_protection_backup_policy_disks_name" {
-  description = "List of name values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.name]
+  description = "Map of name values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.name }
 }
 output "data_protection_backup_policy_disks_retention_rule" {
-  description = "List of retention_rule values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.retention_rule]
+  description = "Map of retention_rule values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.retention_rule }
 }
 output "data_protection_backup_policy_disks_time_zone" {
-  description = "List of time_zone values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.time_zone]
+  description = "Map of time_zone values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.time_zone }
 }
 output "data_protection_backup_policy_disks_vault_id" {
-  description = "List of vault_id values across all data_protection_backup_policy_disks"
-  value       = [for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : v.vault_id]
+  description = "Map of vault_id values across all data_protection_backup_policy_disks, keyed the same as var.data_protection_backup_policy_disks"
+  value       = { for k, v in azurerm_data_protection_backup_policy_disk.data_protection_backup_policy_disks : k => v.vault_id }
 }
 
